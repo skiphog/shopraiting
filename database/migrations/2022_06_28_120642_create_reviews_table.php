e4141c8ee41318ba3394e257bf2034025e310717
Skiphog <skiphog@gmail.com>
2022-06-28 12:13:53 +0300

    Reviews begin

---
+++ database/migrations/2022_06_28_120642_create_reviews_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', static function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('shop_id');
+            $table->unsignedFloat('rating', 4)->default(0.00);
+            $table->string('author_name');
+            $table->string('author_email');
+            $table->text('content');
+            $table->timestamps();
+            $table->boolean('activity')->default(false);
+
+            //Indexes
+            $table->index('activity');
+            $table->foreign('shop_id')
+                ->references('id')
+                ->on('shops');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('reviews');
+    }
+};
