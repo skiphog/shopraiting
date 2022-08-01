59133280a10c47e14e2d1f2456f4d3479c0d2953
Skiphog <skiphog@gmail.com>
2022-08-01 18:21:45 +0300

    fix admin role

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -18,6 +18,12 @@ class UserRequest extends FormRequest
      */
     public function rules(): array
     {
+        $rules_role = ['required', 'integer', Rule::in(array_keys(User::$roles))];
+
+        if (1 === (int)$this->route('user')?->id) {
+            $rules_role[] = 'min:4';
+        }
+
         return [
             'email'       => [
                 'required',
@@ -27,7 +33,15 @@ class UserRequest extends FormRequest
             ],
             'name'        => ['required', 'string', 'max:250'],
             'description' => ['nullable', 'string'],
-            'role'        => ['required', 'integer', Rule::in(array_keys(User::$roles))],
+            'role'        => $rules_role,
         ];
+    }
+
+    /**
+     * @noinspection PhpArrayShapeAttributeCanBeAddedInspection
+     */
+    public function messages(): array
+    {
+        return ['role.min' => 'Супер пользователь не может иметь права ниже администратора'];
     }
 }
