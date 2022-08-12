746bab74e1f4ad1ab6e81321ade61fa06027971c
Skiphog <skiphog@gmail.com>
2022-08-12 15:45:05 +0300

    Small fix. All shops.2

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Shop;
 use App\Models\Review;
 use App\Models\Article;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
