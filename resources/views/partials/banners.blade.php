9ce4b02d3aee44ff18f356a374596d582b2b745e
Skiphog <skiphog@gmail.com>
2022-09-26 13:11:19 +0300

    Fix slider

--- resources/views/partials/banners.blade.php
+++ resources/views/partials/banners.blade.php
@@ -6,11 +6,19 @@
 
 ?>
 @if($banners->isNotEmpty())
-    @foreach($banners as $banner)
-        <div class="banner-wrapper">
-            <a href="{{ url($banner->link) }}" target="_blank" rel="noopener noreferrer">
-                <img src="{{ asset($banner->path) }}" alt="{{ $banner->name }}">
-            </a>
-        </div>
-    @endforeach
+    @push('styles')
+        <link rel="stylesheet" href="/css/slick.css">
+    @endpush
+    @push('scripts')
+        <script src="/js/slider-index.js"></script>
+    @endpush
+    <div class="index__slider">
+        @foreach($banners as $key => $banner)
+            <div class="banner-wrapper {{ $key > 0 ? 'hidden': '' }}" style="visibility:hidden">
+                <a href="{{ url($banner->link) }}" target="_blank" rel="noopener noreferrer">
+                    <img src="{{ asset($banner->path) }}" alt="{{ $banner->name }}">
+                </a>
+            </div>
+        @endforeach
+    </div>
 @endif
